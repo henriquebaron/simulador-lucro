9bc9547ddce849d67f351dc383f8bbdf8bdb99dc
Henrique Baron <henrique.baron@gmail.com>
2021-11-18 20:51:50 +0100

    Utilizar o Newtonsoft Json na API
    Para que a conversão de dados de TimeSpan funcione corretamente.

--- SimuladorLucroAPI/SimuladorLucroAPI/Startup.cs
+++ SimuladorLucroAPI/SimuladorLucroAPI/Startup.cs
@@ -26,7 +26,7 @@ namespace SimuladorLucroAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
 
             services.AddDbContext<SimuladorLucroAPIContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("SimuladorLucroAPIContext")));
