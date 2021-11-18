8b59518036ca796807ff078075812ec71f605787
Henrique Baron <henrique.baron@gmail.com>
2021-11-18 19:46:25 +0100

    Customizar a string de conexão e o DbContext
    Definindo os tipos de dados corretos para cada coluna. Ajuste realizado
    para poder gerar as Migrations do EF Core em seguida.

--- SimuladorLucroAPI/SimuladorLucroAPI/Data/SimuladorLucroAPIContext.cs
+++ SimuladorLucroAPI/SimuladorLucroAPI/Data/SimuladorLucroAPIContext.cs
@@ -15,5 +15,19 @@ namespace SimuladorLucroAPI.Data
         }
 
         public DbSet<SimuladorLucroAPI.Models.Servico> Servico { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Servico>()
+                .Property(p => p.Nome).IsRequired();
+            modelBuilder.Entity<Servico>()
+                .Property(p => p.Descricao).HasColumnType("VARCHAR(300)");
+            modelBuilder.Entity<Servico>()
+                .Property(p => p.Duracao).HasDefaultValue(new TimeSpan(1, 0, 0));
+            modelBuilder.Entity<Servico>()
+                .Property(p => p.Valor).HasColumnType("DECIMAL(5,2)").IsRequired();
+            modelBuilder.Entity<Servico>()
+                .Property(p => p.Custo).HasColumnType("DECIMAL(5,2)").IsRequired();
+        }
     }
 }
