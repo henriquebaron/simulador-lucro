5dd91c11e65f6929352d39ac010f83e5c7b5a54a
Henrique Baron <henrique.baron@gmail.com>
2021-11-24 21:40:47 +0100

    Adicionar classe de modelo para os agendamentos

---
+++ SimuladorLucroAPI/SimuladorLucroAPI/Models/Agendamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimuladorLucroAPI.Models
+{
+    public class Agendamento
+    {
+        public int Id { get; set; }
+        public Servico Servico { get; set; }
+        public string NomeCliente { get; set; }
+        public DateTime DataHora { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
